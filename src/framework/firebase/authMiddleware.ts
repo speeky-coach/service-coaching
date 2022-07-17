892466c416a108d5152a3717b3c3af30334e8220
Grover Lee <bruce.lee@encora.com>
2022-07-16 22:27:08 -0500

    fix error handling auth

--- src/framework/firebase/authMiddleware.ts
+++ src/framework/firebase/authMiddleware.ts
@@ -4,23 +4,27 @@ import { AuthenticatedRequest, UserTokenPayload } from '../express/types';
 import { firebaseAuth } from './FirebaseApp';
 
 async function authMiddleware(request: Request | AuthenticatedRequest, response: Response, next: NextFunction) {
-  if (!request.headers.authorization) throw new UnauthorizedError();
+  try {
+    if (!request.headers.authorization) throw new UnauthorizedError();
 
-  const [type, token] = request.headers.authorization.split(' ');
+    const [type, token] = request.headers.authorization.split(' ');
 
-  if (type !== 'Bearer' || !token) throw new UnauthorizedError();
+    if (type !== 'Bearer' || !token) throw new UnauthorizedError();
 
-  const decodedToken = await firebaseAuth.verifyIdToken(token);
+    const decodedToken = await firebaseAuth.verifyIdToken(token);
 
-  const user: UserTokenPayload = {
-    id: decodedToken.uid,
-    email: decodedToken.email,
-    role: 'student',
-  };
+    const user: UserTokenPayload = {
+      id: decodedToken.uid,
+      email: decodedToken.email,
+      role: 'student',
+    };
 
-  (request as AuthenticatedRequest).user = user;
+    (request as AuthenticatedRequest).user = user;
 
-  next();
+    next();
+  } catch (error) {
+    next(error);
+  }
 }
 
 export default authMiddleware;
